750beac4000f57b9e2fa9d32ee3d62e91df38b24
Michael Hoffmann <mghoffmann@gmail.com>
2020-12-02 19:23:10 -0700

    Added lots of comments.

--- NEO.js
+++ NEO.js
@@ -1,9 +1,31 @@
+// Describes a Near Earth Object and its close approaches to Earth.
+//
+// Fields:
+//  id: A unique ID for this NEO. This works as a key for the NeoWS API too.
+//  name: NASA's given name for this NEO
+//  nasa_jpl_url: The URL to this NEO's NeoWS access point. Do not request this if avoidable: it counts toward the rate limit for the API key used in the Neo API request that retrieved this NEO's constructor data.
+//  absolute_magnitude_h: The absolute magnitude (h) of this NEO. See https://cneos.jpl.nasa.gov/about/neo_groups.html for what that means.
+//  is_potentially_hazardous_asteroid: Whether this NEO is potentially hazardous to Earth.
+//  estimated_diameter_min: The minimum estimated diameter of this NEO
+//  estimated_diameter_max: The maximum estimated diameter of this NEO
+//  approaches: An array of Approach instances describing all of this NEO's approaches NASA knows about or predicts.
+//
+// Static members:
+//  INTERN: A dictionary keyed by NEO id and valued by NEO instances. This is used to avoid making requests to the NeoWS API more than one time for the same NEO,
+//        because the same NEO might be referenced multiple times by the Neo API.
+//  getByID(id): Takes an ID and returns a NEO instance from INTERN or undefined if no instance has yet been constructd with that ID.
 class NEO {
 
     // A dictionary containing each instance of this class, keyed by ID.
     static INTERN = {};
 
-    // Each response from the neo API has a near_earth_objects property,
+    // Returns the NEO instance with the given id, or undefined if no
+    // NEO has been constructed with that ID yet. See constructor.
+    static getByID(id) {
+        return INTERN[id];
+    }
+
+    // Each response from the Neo API has a near_earth_objects property,
     // which is a dictionary keyed by date and valued by arrays of objects.
     // This constructor expects a single one of those objects.
     // i.e. neoResponse.near_earth_objects["1900-01-01"][0]    
@@ -32,12 +54,16 @@ class NEO {
                 return response.json();
             })
             .then(function (neowsJSON) {
-                NEO.INTERN[json.id].approaches = [];                
+                // Construct the approaches using the NeoWS data
+                NEO.INTERN[json.id].approaches = [];
+
                 neowsJSON.close_approach_data.forEach(a => {
                     NEO.INTERN[json.id].approaches.push(new Approach(json.id, a));
-                });                    
+                });
             })
             .catch(function (error) {
+                // TODO: Show the user a message if we hit the rate limit. Or just avoid
+                // hitting the rate limit...
                 console.error(error);
             })
     }
